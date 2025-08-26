9cf2d46df3e784296dffa0de46726a1ed02c2883
lisanalhaque <lisanop4444@gmail.com>
2025-08-26 10:55:06 +0530

    26-08-2025

---
+++ array-methods/accessor-metods/toString.js
@@ -0,0 +1,3 @@
+// The toString() method returns a string representing the specified array and its elements.
+let fruits = ['apple', 'orange', 'banana'];
+console.log(fruits.toString()); // "apple,orange,banana"
