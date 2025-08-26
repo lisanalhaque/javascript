bbe72fd1d24c0a29eceb971c6532f3da39caa13d
lisanalhaque <lisanop4444@gmail.com>
2025-08-26 11:00:44 +0530

    26-08-2025

---
+++ array-methods/accessor-metods/toLocalString.js
@@ -0,0 +1,4 @@
+// The toLocaleString() method returns a string representing the specified array and its elements, using locale-specific conventions.
+ const prices = [1245,56];
+ const data = prices.toLocaleString('en-US');
+ console.log(data); // "1,245.56"
