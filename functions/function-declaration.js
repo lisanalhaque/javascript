b7965728917c4ce56193337af6355a7b99dcbcf6
lisanalhaque <lisanop4444@gmail.com>
2025-09-02 11:05:03 +0530

    02-09-2025

---
+++ functions/function-declaration.js
@@ -0,0 +1,6 @@
+// function declaration is one way to define a function 
+//syntax functionName(parameters) {
+function sayHello( name) {
+  console.log('Hello, ' + name + '!');
+}
+sayHello('abi');//argument
